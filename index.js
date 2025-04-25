57550953136fd2bae635fd6de65f619ccec46837
Janne Mäkinen <jannejmakinen@gmail.com>
2025-04-25 15:58:16 +0300

    bebuild

---
+++ index.js
@@ -0,0 +1,83 @@
+
+// Air Fryer Vault Application 2025 Janne J. Mäkinen
+// backend
+
+// TODO
+// TODO (front and back)
+// add functionality for recipe edit, add, remove
+// limit of showing recipes
+// better looking card
+// search functionality
+// add more requests for testing
+
+require('dotenv').config()  // remember to config .env and .gitignore 
+const express = require('express')
+const Recipe = require('./models/recipe')
+const app = express()
+app.use(express.static('dist'))
+app.use(express.json())         // use this so we can get later body = request.body
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+app.use(requestLogger)
+
+// uses mongo with error middleware
+// get all recipes
+app.get('/api/recipes', (request, response) => {
+    console.log('BE: app.get()')
+    Recipe.find({}).then(recipes => {
+        response.json(recipes)
+    })
+    .catch(error => next(error))
+})
+
+// uses mongo with error middleware
+// add a recipe
+app.post('/api/recipes', (request, response) => {
+    console.log('BE: app.post()')
+    const body = request.body
+    if (!body.name) {
+      return response.status(400).json({ error: 'recipe name missing!' })
+    }
+    const recipe = new Recipe({
+        name: body.name,
+        recipe: body.recipe,
+        ingredients: body.ingredients
+    })
+    recipe.save().then(savedRecipe => {
+        response.json(savedRecipe)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'BE: unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'BE: malformatted id' })
+    }
+    next(error)
+}
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`BE: Server running on port ${PORT}`)
+})
+
+
+
+
+
+
+
+
---
+++ models/recipe.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGO_URI
+
+console.log('BE: connecting to', url)
+mongoose
+    .connect(url)
+    .then(result => {
+    console.log('BE: connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('BE: error connecting to MongoDB:', error.message)
+  })
+
+const recipeSchema = new mongoose.Schema({
+  name: String,
+  recipe: String,
+  ingredients: String,
+})
+
+recipeSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Recipe', recipeSchema)
